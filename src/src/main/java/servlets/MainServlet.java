c03e8ce221e4af47d01a7b2ca6321fcf13968370
AndrewL <herokuluniaka@gmail.com>
2017-05-03 19:50:57 +0300

    First commit

---
+++ src/src/main/java/servlets/MainServlet.java
@@ -0,0 +1,23 @@
+package servlets;
+
+/**
+ * Created by USER on 02.05.2017.
+ */
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        //resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>Hello Servlet</h1>");
+    }
+}
